04de1b7d90d5276a3f3554aa753383a0a19e0707
jtenavidal <jtenavidal@tauex.tau.ac.il>
2024-10-10 03:22:28 -0500

    changing dependencies

--- emMCRadCorr_gpvm_env.sh
+++ emMCRadCorr_gpvm_env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Set up the UPS products needed to build and use GENIE
-source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh && retval="$?"
+source /cvmfs/fermilab.opensciencegrid.org/products/genie/bootstrap_genie_ups.sh 
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup root v6_26_06b -q e26:p3913:prof
 setup cmake v3_23_1 -f Linux64bit+3.10-2.17 
-
-source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups 
 setup ifdhc v2_6_6
 export IFDH_CP_MAXRETRIES=0
 
