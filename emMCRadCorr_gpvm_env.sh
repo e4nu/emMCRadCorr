9dd34a93c8d5cf2aa033dacdf0773a7470289f03
jtenavidal <jtenavidal@tauex.tau.ac.il>
2024-10-11 03:04:09 -0500

    testing different root version

--- emMCRadCorr_gpvm_env.sh
+++ emMCRadCorr_gpvm_env.sh
@@ -3,7 +3,8 @@
 # Set up the UPS products needed to build and use GENIE
 source /cvmfs/fermilab.opensciencegrid.org/products/genie/bootstrap_genie_ups.sh 
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
-setup root v6_26_06b -q e26:p3913:prof
+#setup root v6_26_06b -q e26:p3913:prof
+setup root v6_22_08d -q debug:e20:p392
 setup cmake v3_23_1 -f Linux64bit+3.10-2.17 
 setup ifdhc v2_6_6
 export IFDH_CP_MAXRETRIES=0
